e0ee14d3249449c9b3e039e98b9f0a086c72d660
Matthew Scharley <matt@scharley.me>
2021-12-11 22:30:47 +1100

    chore: minor refactor dev tools

--- src/main/attemptInstallDevTools.ts
+++ src/main/attemptInstallDevTools.ts
@@ -14,24 +14,23 @@ try {
 }
 export const isDev = isDevTmp;
 
-let installExtensions: () => Promise<void>;
+let installExtensions: () => Promise<void> = Promise.resolve.bind(Promise);
 try {
-  if (!isDev) {
-    throw new Error('Loading development extensions while not in dev mode.');
+  if (isDev) {
+    log.info('DEVELOPER MODE: Installing developer extensions.');
+    const {
+      default: installExtension,
+      REACT_DEVELOPER_TOOLS,
+      REDUX_DEVTOOLS,
+    } = require('electron-devtools-installer') as typeof import('electron-devtools-installer');
+    installExtensions = async (): Promise<void> => {
+      await installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS], {
+        loadExtensionOptions: { allowFileAccess: true },
+      });
+    };
   }
-  log.info('DEVELOPER MODE: Installing developer extensions.');
-  const {
-    default: installExtension,
-    REACT_DEVELOPER_TOOLS,
-    REDUX_DEVTOOLS,
-  } = require('electron-devtools-installer') as typeof import('electron-devtools-installer');
-  installExtensions = async (): Promise<void> => {
-    await installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS], {
-      loadExtensionOptions: { allowFileAccess: true },
-    });
-  };
 } catch (e: unknown) {
-  installExtensions = Promise.resolve.bind(Promise);
+  /* do nothing, default is already set */
 }
 
-export const attemptInstallDevTools: () => Promise<void> = installExtensions;
+export const attemptInstallDevTools = installExtensions;
