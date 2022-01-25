7cebf24e0e2006c108ea937d64c71bf070259bdb
Matthew Scharley <matt@scharley.me>
2022-01-26 05:24:23 +1100

    fix: deal with external url's correctly (#22)

--- src/main/MainWindow.ts
+++ src/main/MainWindow.ts
@@ -1,7 +1,9 @@
 import { attemptInstallDevTools } from './attemptInstallDevTools';
+import type { Event } from 'electron/main';
 import { BrowserWindow } from 'electron/main';
 import { injectable } from 'inversify';
 import path from 'path';
+import { shell } from 'electron/common';
 
 @injectable()
 export class MainWindow {
@@ -18,15 +20,46 @@ export class MainWindow {
       height: 768,
       webPreferences: {
         contextIsolation: true,
+        disableBlinkFeatures: 'Auxclick',
         nodeIntegration: false,
         nodeIntegrationInWorker: false,
         /* eng-disable PRELOAD_JS_CHECK */ preload: path.join(__dirname, '../renderer/preload.js'),
         sandbox: true,
       },
     });
+    this._window.webContents.on('will-navigate', (event, url) => {
+      if (!this.isAllowedInternalNavigationUrl(url)) {
+        event.preventDefault();
+        this.tryExternalNavigation(url);
+      }
+    });
+    this._window.webContents.setWindowOpenHandler((details) => {
+      if (this.isAllowedInternalNavigationUrl(details.url)) {
+        return { action: 'allow' };
+      } else {
+        this.tryExternalNavigation(details.url);
+        return { action: 'deny' };
+      }
+    });
     this._window.on('closed', this.onClose);
 
     await this._window.loadURL('app://renderer/index.html');
+  };
+
+  private readonly isAllowedInternalNavigationUrl = (url: string): boolean => {
+    const parsed = new URL(url);
+
+    return parsed.protocol === 'app:';
+  };
+
+  private readonly tryExternalNavigation = (url: string): void => {
+    const parsed = new URL(url);
+
+    if (['http:', 'https:', 'mailto:'].includes(parsed.protocol)) {
+      /* eng-disable OPEN_EXTERNAL_JS_CHECK */ shell.openExternal(url).catch((e) => log.error(e));
+    } else {
+      log.warn(`Invalid external URL attempted to open: ${url}`);
+    }
   };
 
   private readonly onClose = (): void => {
