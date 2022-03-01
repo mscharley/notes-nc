4bd2d0bc234745d18d872b8b8349c14cfa03dfcd
Matthew Scharley <matt@scharley.me>
2022-03-01 14:31:44 +1100

    chore: add tests

--- src/main/services/AboutElectron.ts
+++ src/main/services/AboutElectron.ts
@@ -1,8 +1,8 @@
 import { ElectronApp, ElectronIpcMain } from '../inversify/tokens';
+import { injectable, unmanaged } from 'inversify';
 import type { AboutDetails } from '@shared/model/AboutDetails';
 import { compare } from 'compare-versions';
 import { DevTools } from './DevTools';
-import { injectable } from 'inversify';
 import { injectToken } from 'inversify-token';
 import type { OnReadyHandler } from '../interfaces/OnReadyHandler';
 import { UpdatesProvider } from './UpdatesProvider';
@@ -14,6 +14,7 @@ export class AboutElectron implements OnReadyHandler {
     @injectToken(ElectronIpcMain) private readonly ipcMain: ElectronIpcMain,
     private readonly updates: UpdatesProvider,
     private readonly devtools: DevTools,
+    @unmanaged() private readonly process: NodeJS.Process = globalThis.process,
   ) {}
 
   public readonly onAppReady = (): void => {
@@ -22,15 +23,15 @@ export class AboutElectron implements OnReadyHandler {
       const version = this.app.getVersion();
 
       return {
-        electronVersion: process.versions.electron,
+        electronVersion: this.process.versions.electron,
         version,
         isDevBuild: this.devtools.isDev,
 
         updateExists: compare(updateCheckResults?.updateInfo.version ?? '0.0.0', version, '>'),
         updateVersion: updateCheckResults?.updateInfo.version,
 
-        osName: process.platform,
-        osVersion: process.getSystemVersion(),
+        osName: this.process.platform,
+        osVersion: this.process.getSystemVersion(),
       };
     });
   };
