c3cd8f67339bb3c53fb00353400f41cfca718b85
Matthew Scharley <matt@scharley.me>
2022-03-01 14:53:37 +1100

    fix: auto-updater always displays that there is an update (#77)

--- src/main/services/AboutElectron.ts
+++ src/main/services/AboutElectron.ts
@@ -1,7 +1,8 @@
 import { ElectronApp, ElectronIpcMain } from '../inversify/tokens';
+import { injectable, unmanaged } from 'inversify';
 import type { AboutDetails } from '@shared/model/AboutDetails';
+import { compare } from 'compare-versions';
 import { DevTools } from './DevTools';
-import { injectable } from 'inversify';
 import { injectToken } from 'inversify-token';
 import type { OnReadyHandler } from '../interfaces/OnReadyHandler';
 import { UpdatesProvider } from './UpdatesProvider';
@@ -13,22 +14,24 @@ export class AboutElectron implements OnReadyHandler {
     @injectToken(ElectronIpcMain) private readonly ipcMain: ElectronIpcMain,
     private readonly updates: UpdatesProvider,
     private readonly devtools: DevTools,
+    @unmanaged() private readonly process: NodeJS.Process = globalThis.process,
   ) {}
 
   public readonly onAppReady = (): void => {
     this.ipcMain.handle('about-details', async (): Promise<AboutDetails> => {
       const updateCheckResults = await this.updates.checkResults;
+      const version = this.app.getVersion();
 
       return {
-        electronVersion: process.versions.electron,
-        version: this.app.getVersion(),
+        electronVersion: this.process.versions.electron,
+        version,
         isDevBuild: this.devtools.isDev,
 
-        updateExists: updateCheckResults?.updateInfo.version != null,
+        updateExists: compare(updateCheckResults?.updateInfo.version ?? '0.0.0', version, '>'),
         updateVersion: updateCheckResults?.updateInfo.version,
 
-        osName: process.platform,
-        osVersion: process.getSystemVersion(),
+        osName: this.process.platform,
+        osVersion: this.process.getSystemVersion(),
       };
     });
   };
