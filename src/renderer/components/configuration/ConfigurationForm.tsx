7b957b0eecc235f24f80759111ab33d303333172
Matthew Scharley <matt@scharley.me>
2022-03-10 00:26:31 +1100

    fix: use a string enum

--- src/renderer/components/configuration/ConfigurationForm.tsx
+++ src/renderer/components/configuration/ConfigurationForm.tsx
@@ -10,9 +10,9 @@ import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 
 export enum TabOptions {
-  FolderManagement,
-  Linux,
-  TBC,
+  FolderManagement = 'Folder management',
+  Linux = 'Linux',
+  TBC = 'TBC',
 }
 
 export interface ConfigurationFormProps {
@@ -38,21 +38,21 @@ export const ConfigurationForm: React.FC<ConfigurationFormProps> = ({ initialTab
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={selectedTab} onChange={(_ev, v: TabOptions): void => setSelectedTab(v)}>
           <Tab label='Note folders' value={TabOptions.FolderManagement} />
-          {isLinux ? <Tab label='Linux' value={TabOptions.Linux} /> : <></>}
+          {isLinux ? <Tab label='Linux' value={TabOptions.Linux} /> : null}
           <Tab label='TBC' value={TabOptions.TBC} />
         </Tabs>
       </Box>
-      <TabPanel name={TabOptions.FolderManagement.toString()} hidden={selectedTab !== TabOptions.FolderManagement}>
+      <TabPanel name={TabOptions.FolderManagement} hidden={selectedTab !== TabOptions.FolderManagement}>
         <FolderTab />
       </TabPanel>
-      <TabPanel name={TabOptions.Linux.toString()} hidden={selectedTab !== TabOptions.Linux}>
+      <TabPanel name={TabOptions.Linux} hidden={selectedTab !== TabOptions.Linux}>
         <Box sx={{ p: 3 }}>
           <Button variant='contained' onClick={doInstallation}>
             Install desktop integration
           </Button>
         </Box>
       </TabPanel>
-      <TabPanel name={TabOptions.TBC.toString()} hidden={selectedTab !== TabOptions.TBC}>
+      <TabPanel name={TabOptions.TBC} hidden={selectedTab !== TabOptions.TBC}>
         <Box sx={{ p: 3 }}>Extra configuration coming here soon!</Box>
       </TabPanel>
     </Box>
