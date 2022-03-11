71cc0cc89aa28a60b818f469a5934ef333e0c129
Matthew Scharley <matt@scharley.me>
2022-03-11 22:09:37 +1100

    feat: focus editor if clicking below it

--- src/renderer/components/MarkdownEditor.tsx
+++ src/renderer/components/MarkdownEditor.tsx
@@ -2,8 +2,10 @@ import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 import Box from '@mui/material/Box';
 import CodeMirror from '@uiw/react-codemirror';
 import { languages } from '@codemirror/language-data';
+import type { ReactCodeMirrorRef } from '@uiw/react-codemirror';
 import { setFatalError } from '~renderer/redux';
 import { useAppDispatch } from '~renderer/hooks';
+import { useRef } from 'react';
 
 export interface MarkdownEditorProps {
   value: string;
@@ -12,9 +14,16 @@ export interface MarkdownEditorProps {
 
 export const MarkdownEditor: React.FC<MarkdownEditorProps> = ({ onChange, value }) => {
   const dispatch = useAppDispatch();
+  const codemirror = useRef<ReactCodeMirrorRef | null>(null);
+
+  const handleClick: React.MouseEventHandler<HTMLDivElement> = (ev) => {
+    if (ev.target !== codemirror.current?.editor) {
+      codemirror.current?.view?.focus();
+    }
+  };
 
   return (
-    <Box sx={{ paddingBottom: 'calc(100vh - 1.7em)' }}>
+    <Box sx={{ paddingBottom: 'calc(100vh - 1.7em)' }} onClick={handleClick}>
       <CodeMirror
         extensions={[markdown({ base: markdownLanguage, codeLanguages: languages })]}
         onChange={(s): void => {
@@ -23,6 +32,7 @@ export const MarkdownEditor: React.FC<MarkdownEditorProps> = ({ onChange, value 
           });
         }}
         value={value}
+        ref={codemirror}
       />
     </Box>
   );
